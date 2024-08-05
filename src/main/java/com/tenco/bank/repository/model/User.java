d98489ef1aa5a261c8d1bf1a4692a73e9f55b11a
bbangEor <eor1402@naver.com>
2024-08-05 11:19:43 +0900

    테이블 설계 모델링

---
+++ src/main/java/com/tenco/bank/repository/model/User.java
@@ -0,0 +1,25 @@
+package com.tenco.bank.repository.model;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class User {
+	private Integer id;
+	private String username;
+	private String password;
+	private String fullname;
+	private Timestamp createdAt;
+	
+	
+}
